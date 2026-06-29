af668f41797069c5813a78353c00869345b2b0ae
Yuri Sabedot Venturin <ysventurin@ucs.br>
2026-06-29 00:36:36 -0300

    feat: add Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,18 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "John", "john@mail.com", LocalDate.of(2026, 6, 29), 3300.0, obj);
+        System.out.println(seller);
 
         Connection con = DB.getConnection();
         DB.closeConnection();
