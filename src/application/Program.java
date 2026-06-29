2665c1b4af0c6566607c4ca5caaeb2facfff3261
Yuri Sabedot Venturin <ysventurin@ucs.br>
2026-06-28 21:57:01 -0300

    feat: add Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,15 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
 public class Program {
     public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
         Connection con = DB.getConnection();
         DB.closeConnection();
     }
